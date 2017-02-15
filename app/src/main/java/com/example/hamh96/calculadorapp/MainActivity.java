420ea7b86ab0a8bed3eda7a73c7ad7888511bb16
HugoM <hmoraleshoyos@gmail.com>
2017-02-15 18:18:27 -0500

    Graphic Interface

---
+++ app/src/main/java/com/example/hamh96/calculadorapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.hamh96.calculadorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void writeB7(View view) {
+
+    }
+
+    public void writeB8(View view) {
+    }
+
+    public void writeB9(View view) {
+    }
+
+    public void writeBplus(View view) {
+    }
+
+    public void writeB4(View view) {
+    }
+
+    public void writeB5(View view) {
+    }
+
+    public void writeB6(View view) {
+    }
+
+    public void writeBsustract(View view) {
+    }
+
+    public void writeB1(View view) {
+    }
+
+    public void writeB2(View view) {
+    }
+
+    public void writeB3(View view) {
+    }
+
+    public void writeBmulti(View view) {
+    }
+
+    public void writeBdot(View view) {
+    }
+
+    public void writeB0(View view) {
+    }
+
+    public void writeBC(View view) {
+    }
+
+    public void writeBdivide(View view) {
+    }
+
+    public void writeBequal(View view) {
+    }
+}
